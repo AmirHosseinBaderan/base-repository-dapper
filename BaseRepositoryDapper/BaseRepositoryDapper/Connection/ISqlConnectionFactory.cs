84ec3ceaaf28c054adec41a4e4545f65c01f8abc
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2024-07-14 17:58:49 +0330

    create : sql connection factory

---
+++ BaseRepositoryDapper/BaseRepositoryDapper/Connection/ISqlConnectionFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace BaseRepositoryDapper.Connection;
+
+public interface ISqlConnectionFactory
+{
+    IDbConnection GetOpenConnection();
+}
---
+++ BaseRepositoryDapper/BaseRepositoryDapper/Connection/SqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace BaseRepositoryDapper.Connection;
+
+public class SqlConnectionFactory(string connectionString) : ISqlConnectionFactory, IDisposable
+{
+    private IDbConnection _connection;
+
+    public void Dispose()
+    {
+        if (_connection is { State: ConnectionState.Open }) _connection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public IDbConnection GetOpenConnection()
+    {
+        if (_connection is not { State: ConnectionState.Open })
+        {
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+        }
+
+        return _connection;
+    }
+}
